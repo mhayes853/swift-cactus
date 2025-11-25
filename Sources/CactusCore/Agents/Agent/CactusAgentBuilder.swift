0ba9a4fb9d8363050fa3287007c362750c81e352
mhayes853 <matthewha853@gmail.com>
2025-11-25 15:02:41 -0800

    Get rid of chained agent for now

--- Sources/CactusCore/Agents/Agent/CactusAgentBuilder.swift
+++ Sources/CactusCore/Agents/Agent/CactusAgentBuilder.swift
@@ -1,28 +1,6 @@
 @resultBuilder
 public enum CactusAgentBuilder<Input: CactusPromptRepresentable, Output: ConvertibleFromJSONValue> {
-  public static func buildArray<A: CactusAgent<Input, Output>>(
-    _ components: [A]
-  ) -> _ChainedAgent<A> {
-    _ChainedAgent(components)
-  }
-
   public static func buildBlock() -> EmptyAgent<Input, Output> {
     EmptyAgent()
-  }
-}
-
-public struct _ChainedAgent<Agent: CactusAgent>: CactusAgent {
-  let agents: [Agent]
-
-  init(_ agents: [Agent]) {
-    self.agents = agents
-  }
-
-  public func stream(
-    isolation: isolated (any Actor)?,
-    input: Agent.Input,
-    into continuation: CactusAgentStream<Agent.Output>.Continuation
-  ) async throws {
-    fatalError()
   }
 }
