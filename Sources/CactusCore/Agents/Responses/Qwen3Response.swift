53fbc1cf13b063eb6890ce7244c5624e5b261729
mhayes853 <matthewha853@gmail.com>
2025-11-26 01:56:39 -0800

    Qwen3Response

---
+++ Sources/CactusCore/Agents/Responses/Qwen3Response.swift
@@ -0,0 +1,44 @@
+public struct Qwen3Response<Base: ConvertibleFromCactusResponse>: ConvertibleFromCactusResponse {
+  public let thinkingContent: String?
+  public let response: Base
+
+  public var promptContent: CactusPromptContent {
+    get throws(Base.PromptContentFailure) {
+      let baseContent = try self.response.promptContent
+      return CactusPromptContent {
+        GroupContent {
+          if let thinkingContent {
+            "<think>\n\(thinkingContent)\n</think>"
+          }
+          baseContent
+        }
+        .separated(by: "\n\n")
+      }
+    }
+  }
+
+  public init(cactusResponse: String) throws(Base.ConversionFailure) {
+    let matches = thinkingContentRegex.matchGroups(from: cactusResponse)
+    if matches.isEmpty {
+      self.thinkingContent = nil
+      self.response = try Base(cactusResponse: cactusResponse)
+    } else {
+      self.thinkingContent = String(matches[0])
+      let baseResponse = matches.count > 1 ? matches[1] : ""
+      self.response = try Base(cactusResponse: String(baseResponse))
+    }
+  }
+
+  public init(thinkingContent: String? = nil, response: Base) {
+    self.thinkingContent = thinkingContent
+    self.response = response
+  }
+}
+
+extension Qwen3Response: Sendable where Base: Sendable {}
+extension Qwen3Response: Equatable where Base: Equatable {}
+extension Qwen3Response: Hashable where Base: Hashable {}
+
+private let thinkingContentRegex = try! RegularExpression(
+  "<think>\n([\\s\\S]*?)\n<\\/think>\n\n([\\s\\S]*)"
+)
