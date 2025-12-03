f5ece4b45ba60e0d2e6f251791043ea1792a56f1
mhayes853 <matthewha853@gmail.com>
2025-12-03 00:01:25 -0800

    Move CactusResponse

--- Sources/CactusCore/Agents/Response/FullResponseParsing.swift
+++ Sources/CactusCore/Agents/Response/FullResponseParsing.swift
@@ -1,3 +1,15 @@
+// MARK: - CactusResponse
+
+public struct CactusResponse: Hashable, Sendable, Identifiable {
+  public let id: CactusGenerationID
+  public let content: String
+
+  public init(id: CactusGenerationID, content: String) {
+    self.id = id
+    self.content = content
+  }
+}
+
 // MARK: - ConvertibleFromCactusResponse
 
 public protocol ConvertibleFromCactusResponse {
