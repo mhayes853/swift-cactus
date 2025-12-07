c73b6a4f465e7e4ecc694068e45d042f918fe7c3
mhayes853 <matthewha853@gmail.com>
2025-12-07 01:14:17 -0800

    Remove transcript environment value

--- Sources/CactusCore/Agents/CactusTranscript.swift
+++ Sources/CactusCore/Agents/CactusTranscript.swift
@@ -1,23 +1,5 @@
 // MARK: - CactusTranscript
 
 public struct CactusTranscript: Hashable, Sendable, Codable {
-}
-
-// MARK: - Environment
-
-extension CactusAgent {
-  public func transcript(_ transcript: CactusTranscript) -> _TransformEnvironmentAgent<Self> {
-    self.environment(\.transcript, transcript)
-  }
-}
-
-extension CactusEnvironmentValues {
-  public var transcript: CactusTranscript {
-    get { self[TranscriptKey.self] }
-    set { self[TranscriptKey.self] = newValue }
-  }
-
-  private enum TranscriptKey: Key {
-    static let defaultValue = CactusTranscript()
-  }
+  public init() {}
 }
