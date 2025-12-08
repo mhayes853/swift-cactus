67139fdbe4e892f2577f669156f19e799b843869
mhayes853 <matthewha853@gmail.com>
2025-12-08 01:34:08 -0800

    Update CactusAgentStream API to support multiple agent outputs via a tagging syste,

--- Sources/CactusCore/Agents/CactusAgentStream.swift
+++ Sources/CactusCore/Agents/CactusAgentStream.swift
@@ -16,32 +16,45 @@ public struct CactusAgentStream<Output: ConvertibleFromCactusResponse>: Sendable
   public func stop() {}
 }
 
-// MARK: - OnResponseUpdate
+// MARK: - Partials
 
 extension CactusAgentStream where Output.Partial: ConvertibleFromCactusTokenStream {
-  public func onResponseUpdate(
+  public struct Partials: AsyncSequence {
+    public struct AsyncIterator: AsyncIteratorProtocol {
+      public func next() async throws -> Output.Partial? {
+        fatalError()
+      }
+
+      @available(iOS 18.0, macOS 15.0, tvOS 18.0, watchOS 11.0, visionOS 2.0, *)
+      public func next(isolation actor: isolated (any Actor)?) async throws -> Output.Partial? {
+        nil
+      }
+    }
+
+    public func makeAsyncIterator() -> AsyncIterator {
+      AsyncIterator()
+    }
+  }
+
+  public var finalResponsePartials: Partials {
+    Partials()
+  }
+
+  public func onFinalResponsePartial(
     perform operation: (Result<Output.Partial, any Error>) -> Void
   ) -> CactusSubscription {
     CactusSubscription {}
   }
-}
 
-// MARK: - AsyncSequence
-
-extension CactusAgentStream: AsyncSequence where Output.Partial: ConvertibleFromCactusTokenStream {
-  public struct AsyncIterator: AsyncIteratorProtocol {
-    public mutating func next() async throws -> Output.Partial? {
-      fatalError()
-    }
-
-    @available(iOS 18.0, macOS 15.0, tvOS 18.0, watchOS 11.0, visionOS 2.0, *)
-    public func next(isolation actor: isolated (any Actor)?) async throws -> Output.Partial? {
-      nil
-    }
+  public func responsePartials(tag: some Hashable) -> Partials? {
+    nil
   }
 
-  public func makeAsyncIterator() -> AsyncIterator {
-    AsyncIterator()
+  public func onResponsePartial(
+    tag: some Hashable,
+    perform operation: (Result<Output.Partial, any Error>) -> Void
+  ) -> CactusSubscription? {
+    nil
   }
 }
 
@@ -67,14 +80,25 @@ extension CactusAgentStream {
     }
   }
 
-  public var tokens: Tokens {
+  public var finalResponseTokens: Tokens {
     Tokens()
   }
 
-  public func onToken(
+  public func onFinalResponseToken(
     perform operation: (Result<CactusStreamedToken, any Error>) -> Void
   ) -> CactusSubscription {
     CactusSubscription {}
+  }
+
+  public func tokens(tag: some Hashable) -> Tokens? {
+    nil
+  }
+
+  public func onResponseToken(
+    tag: some Hashable,
+    perform operation: (Result<CactusStreamedToken, any Error>) -> Void
+  ) -> CactusSubscription? {
+    nil
   }
 }
 
