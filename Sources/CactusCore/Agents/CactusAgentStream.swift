030c2f42d504dce958a29e6a8c644517911295be
mhayes853 <matthewha853@gmail.com>
2025-12-08 23:15:56 -0800

    Update Stream Interface

--- Sources/CactusCore/Agents/CactusAgentStream.swift
+++ Sources/CactusCore/Agents/CactusAgentStream.swift
@@ -19,25 +19,8 @@ public struct CactusAgentStream<Output: ConvertibleFromCactusResponse>: Sendable
 // MARK: - Partials
 
 extension CactusAgentStream where Output.Partial: ConvertibleFromCactusTokenStream {
-  public struct Partials: AsyncSequence {
-    public struct AsyncIterator: AsyncIteratorProtocol {
-      public func next() async throws -> Output.Partial? {
-        fatalError()
-      }
-
-      @available(iOS 18.0, macOS 15.0, tvOS 18.0, watchOS 11.0, visionOS 2.0, *)
-      public func next(isolation actor: isolated (any Actor)?) async throws -> Output.Partial? {
-        nil
-      }
-    }
-
-    public func makeAsyncIterator() -> AsyncIterator {
-      AsyncIterator()
-    }
-  }
-
-  public var finalResponsePartials: Partials {
-    Partials()
+  public var finalResponsePartials: CactusAgentStreamPartials<Output.Partial> {
+    CactusAgentStreamPartials()
   }
 
   public func onFinalResponsePartial(
@@ -45,43 +28,48 @@ extension CactusAgentStream where Output.Partial: ConvertibleFromCactusTokenStre
   ) -> CactusSubscription {
     CactusSubscription {}
   }
+}
 
-  public func responsePartials(tag: some Hashable) -> Partials? {
+extension CactusAgentStream {
+  public func responsePartials<Partial: ConvertibleFromCactusTokenStream>(
+    tag: some Hashable,
+    as type: Partial.Type
+  ) -> CactusAgentStreamPartials<Partial>? {
     nil
   }
 
-  public func onResponsePartial(
+  public func onResponsePartial<Partial: ConvertibleFromCactusTokenStream>(
     tag: some Hashable,
-    perform operation: (Result<Output.Partial, any Error>) -> Void
+    as type: Partial.Type,
+    perform operation: (Result<Partial, any Error>) -> Void
   ) -> CactusSubscription? {
     nil
+  }
+
+}
+
+public struct CactusAgentStreamPartials<Partial: ConvertibleFromCactusTokenStream>: AsyncSequence {
+  public struct AsyncIterator: AsyncIteratorProtocol {
+    public func next() async throws -> Partial? {
+      fatalError()
+    }
+
+    @available(iOS 18.0, macOS 15.0, tvOS 18.0, watchOS 11.0, visionOS 2.0, *)
+    public func next(isolation actor: isolated (any Actor)?) async throws -> Partial? {
+      nil
+    }
+  }
+
+  public func makeAsyncIterator() -> AsyncIterator {
+    AsyncIterator()
   }
 }
 
 // MARK: - Tokens
 
 extension CactusAgentStream {
-  public struct Tokens: AsyncSequence {
-    public struct AsyncIterator: AsyncIteratorProtocol {
-      public func next() async throws -> CactusStreamedToken? {
-        nil
-      }
-
-      @available(iOS 18.0, macOS 15.0, tvOS 18.0, watchOS 11.0, visionOS 2.0, *)
-      public func next(
-        isolation actor: isolated (any Actor)?
-      ) async throws -> CactusStreamedToken? {
-        nil
-      }
-    }
-
-    public func makeAsyncIterator() -> AsyncIterator {
-      AsyncIterator()
-    }
-  }
-
-  public var finalResponseTokens: Tokens {
-    Tokens()
+  public var finalResponseTokens: CactusAgentStreamTokens {
+    CactusAgentStreamTokens()
   }
 
   public func onFinalResponseToken(
@@ -90,7 +78,7 @@ extension CactusAgentStream {
     CactusSubscription {}
   }
 
-  public func tokens(tag: some Hashable) -> Tokens? {
+  public func tokens(tag: some Hashable) -> CactusAgentStreamTokens? {
     nil
   }
 
@@ -99,6 +87,25 @@ extension CactusAgentStream {
     perform operation: (Result<CactusStreamedToken, any Error>) -> Void
   ) -> CactusSubscription? {
     nil
+  }
+}
+
+public struct CactusAgentStreamTokens: AsyncSequence {
+  public struct AsyncIterator: AsyncIteratorProtocol {
+    public func next() async throws -> CactusStreamedToken? {
+      nil
+    }
+
+    @available(iOS 18.0, macOS 15.0, tvOS 18.0, watchOS 11.0, visionOS 2.0, *)
+    public func next(
+      isolation actor: isolated (any Actor)?
+    ) async throws -> CactusStreamedToken? {
+      nil
+    }
+  }
+
+  public func makeAsyncIterator() -> AsyncIterator {
+    AsyncIterator()
   }
 }
 
