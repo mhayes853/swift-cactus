1833c64894a927f1010f56b58c9266a3eb6a2020
mhayes853 <matthewha853@gmail.com>
2025-12-10 15:32:27 -0800

    Passthrough Agent Stub

---
+++ Sources/CactusCore/Agents/PassthroughAgent.swift
@@ -0,0 +1,28 @@
+public struct PassthroughAgent<Input: Sendable, Child: CactusAgent>: CactusAgent
+where Child.Input == Input {
+  private let child: Child
+
+  public init(@CactusAgentBuilder<Child.Input, Child.Output> child: () -> Child) {
+    self.child = child()
+  }
+
+  public func build(
+    graph: inout CactusAgentGraph,
+    at nodeId: CactusAgentGraph.Node.ID,
+    in environment: CactusEnvironmentValues
+  ) {
+    let node = graph.appendChild(
+      to: nodeId,
+      fields: CactusAgentGraph.Node.Fields(label: "PassthroughAgent")
+    )
+    guard let node else { return unableToAddGraphNode() }
+    self.child.build(graph: &graph, at: node.id, in: environment)
+  }
+
+  public nonisolated(nonsending) func stream(
+    request: CactusAgentRequest<Input>,
+    into continuation: CactusAgentStream<Input>.Continuation
+  ) async throws -> CactusAgentStream<Input>.Response {
+    .finalOutput(request.input)
+  }
+}
